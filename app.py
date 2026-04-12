fcb560f548a391b26cd0c00817104d558c1fdfdb
tiroplatoburgos <tiroplato.burgos@gmail.com>
2026-04-12 22:55:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import pandas as pd
 # ==========================================
 TITULO_TIRADA = "I TIRADA GRAN PREMIO DE ESPAÑA"
 FECHA_TIRADA = "12 de Abril de 2026"
-URL_BANNER_TOP = "https://via.placeholder.com/1200x200.png?text=TU+PUBLICIDAD+AQUÍ" # Cambia por tu URL
-URL_BANNER_SIDE = "https://tiroolimpicoburgos.es/wp-content/uploads/2022/12/cropped-logo.png"
+URL_BANNER_TOP = "C:\Users\Usuario\Pictures\Icono tiro" # Cambia por tu URL
+URL_BANNER_SIDE = "https://tiroolimpicoburgos.es/wp-content/uploads/2022/12/cropped-logo.png" # Cambia por tu URL
 
 # ==========================================
 # CONFIGURACIÓN DE PÁGINA Y ESTILO
